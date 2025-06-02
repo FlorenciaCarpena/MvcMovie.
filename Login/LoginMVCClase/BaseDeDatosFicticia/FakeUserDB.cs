9276b1c2aea22745066fc3956003910e2086fdbc
FlorenciaCarpena <florenciacarpena@hotmail.com>
2025-06-01 22:21:04 -0300

    Semana 6 Login

---
+++ Login/LoginMVCClase/BaseDeDatosFicticia/FakeUserDB.cs
@@ -0,0 +1,23 @@
+﻿using LoginMVCClase.Models;
+
+namespace LoginMVCClase.BaseDeDatosFicticia
+{
+    public class FakeUserDB
+    {
+        public static List<LoginModel> Users = new List<LoginModel>
+        {
+            new LoginModel
+            {
+                Id = 1,
+                User="admin",
+                Password="12345"
+             },
+            new LoginModel
+            {
+                Id=2,
+                User="admin",
+                Password="1234"
+            }
+        };
+    }
+}
---
+++ Login/LoginMVCClase/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Claims;
+using LoginMVCClase.BaseDeDatosFicticia;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginMVCClase.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login (String username, string password) 
+        {
+            var user = FakeUserDB.Users.FirstOrDefault(u => u.User == username && u.Password == password);
+
+            if (user != null)
+
+            { 
+                
+                    var claims=new List<Claim>
+                    {
+                        new Claim (ClaimTypes.Name, username)
+                    }
+                
+                ;
+              HttpContext.Session.SetString("User", user.User);
+                var identity = new ClaimsIdentity(claims, "Cookies");
+                var principal = new ClaimsPrincipal(identity);
+
+                HttpContext.SignInAsync("Cookies", principal);
+
+                return RedirectToAction("Index", "Home");
+
+            }
+            else
+            {
+                ViewBag.Error = "Credenciales Invalidad";
+            }
+                return View();
+
+        }
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
