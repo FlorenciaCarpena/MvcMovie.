a60a21e85bab512b90e781b91b5632cd177075c0
FlorenciaCarpena <florenciacarpena@hotmail.com>
2025-06-24 15:03:49 -0300

    semana 8

---
+++ Login/LoginMVCClase/Controllers/UsuarioController.cs
@@ -0,0 +1,37 @@
+﻿using LoginMVCClase.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+namespace LoginMVCClase.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult formethod()
+        {
+            UsuarioModel usuario = new UsuarioModel();
+            List<UsuarioModel> listUsuario = new List<UsuarioModel>();
+            for (int i = 0; i < 5; i++) 
+            {
+                usuario.email = "florenciacarpena@gmail.com";
+                usuario.username = "FCarpena";
+                usuario.password = "password";
+                usuario.fechadenacimiento_ = DateTime.Now;
+                usuario.name = "Florencia Carpena";
+                usuario.id = i;
+
+                listUsuario.Add(usuario);
+
+
+            }
+
+            ViewBag.ListaUsuario = listUsuario;
+
+                return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Login/LoginMVCClase/Models/UsuarioModel.cs
@@ -0,0 +1,22 @@
+﻿namespace LoginMVCClase.Models
+{
+    public class UsuarioModel
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+
+        public string name { get; set; }
+
+        public string password { get; set; }
+
+        public DateTime fechadenacimiento_ { get; set; }
+
+
+
+
+        
+
+
+    }
+}
